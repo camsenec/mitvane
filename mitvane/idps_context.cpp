ee052ef5132fa7f355d393630dc98848c3abdfd2
Tomoya Tanaka <totana@ttu.ee>
2022-03-05 21:14:53 +0200

    Add IDS/IPS context
    Added IDS/IPS context to be used as a data store that connects core
    part of IDS/IPS with link layer.

---
+++ mitvane/idps_context.cpp
@@ -0,0 +1,91 @@
+/* 
+ * (C) 2022 Tomoya Tanaka <deepsky2221@gmail.com> 
+ *
+ * This file is part of Mitvane.
+ *
+ * Mitvane is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or 
+ * any later version.
+ * Mitvane is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License and 
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * and GNU Lesser General Public License along with Mitvane. 
+ * If not, see <https://www.gnu.org/licenses/>. 
+ */
+
+
+#include "ethernet_device.hpp"
+#include "idps_context.hpp"
+#include "mitvane/app_layer_parser/application_parser.hpp"
+#include "mitvane/decoder/geonet_decoder.hpp"
+#include "mitvane/classifier/classifier.hpp"
+#include "mitvane/detector/detection_context.hpp"
+#include <vanetza/common/byte_order.hpp>
+#include <iostream>
+
+
+IdpsContext::IdpsContext(const vanetza::geonet::MIB& mib, vanetza::PositionProvider& positioning) :
+    mib_(mib), positioning_(positioning) {}
+
+IdpsContext::~IdpsContext()
+{
+    for (auto* app : app_parsers_) {
+        disable(app);
+    }
+}
+
+void IdpsContext::set_link_layer(LinkLayer* link_layer)
+{
+    namespace sph = std::placeholders;
+
+    if (link_layer) {
+        link_layer->indicate(std::bind(&IdpsContext::run, this, sph::_1, sph::_2));
+    }
+}
+
+void IdpsContext::set_application_layer_parser(vanetza::btp::port_type port, ApplicationParser* handler)
+{
+    m_app_layer_parsers[port] = handler;
+}
+
+void IdpsContext::run(vanetza::CohesivePacket&& packet, const vanetza::EthernetHeader& hdr)
+{
+    if (hdr.source != mib_.itsGnLocalGnAddr.mid() && hdr.type == vanetza::access::ethertype::GeoNetworking) {
+        std::cout << "received packet from " << hdr.source << " (" << packet.size() << " bytes)\n";
+        mitvane::DetectionContext detection_context;
+
+        // 1. Decode
+        std::unique_ptr<vanetza::PacketVariant> up_dec { new vanetza::PacketVariant(packet) };
+        mitvane::GeonetDecoder decoder(mib_, detection_context);
+        decoder.decode(std::move(up_dec), hdr.source, hdr.destination);
+        
+        // 2. Classify and get handler
+        mitvane::Classifier classifier(detection_context);
+        ApplicationParser* handler = classifier.classify(m_app_layer_parsers);
+
+        // 3. Apply application layer parser
+        handler->parse(std::unique_ptr<vanetza::PacketVariant>(std::move(detection_context.payload)));
+    }
+
+}
+
+void IdpsContext::enable(ApplicationParser* app)
+{
+    if (app->port() != vanetza::btp::port_type(0)) {
+        set_application_layer_parser(app->port(), app);
+    }
+}
+
+void IdpsContext::disable(ApplicationParser* app)
+{
+    if (app->port() != vanetza::btp::port_type(0)) {
+        set_application_layer_parser(app->port(), nullptr);
+    }
+}
+
+
---
+++ mitvane/idps_context.hpp
@@ -0,0 +1,59 @@
+/* 
+ * (C) 2022 Tomoya Tanaka <deepsky2221@gmail.com> 
+ *
+ * This file is part of Mitvane.
+ *
+ * Mitvane is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or 
+ * any later version.
+ * Mitvane is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License and 
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * and GNU Lesser General Public License along with Mitvane. 
+ * If not, see <https://www.gnu.org/licenses/>. 
+ */
+
+
+#ifndef IDPS_CONTEXT_HPP
+#define IDPS_CONTEXT_HPP
+
+#include "link_layer.hpp"
+#include "mitvane/app_layer_parser/application_parser.hpp"
+#include <vanetza/common/position_provider.hpp>
+#include <vanetza/btp/header.hpp>
+#include <vanetza/geonet/mib.hpp>
+#include <array>
+#include <list>
+#include <memory>
+
+
+
+class IdpsContext
+{
+public:
+    IdpsContext(const vanetza::geonet::MIB&, vanetza::PositionProvider&);
+    ~IdpsContext();
+    void set_link_layer(LinkLayer*);
+    void set_application_layer_parser(vanetza::btp::port_type port, ApplicationParser* handler);
+    void enable(ApplicationParser*);
+    void disable(ApplicationParser*);
+
+private:
+    void run(vanetza::CohesivePacket&&, const vanetza::EthernetHeader&);
+    void decode(vanetza::CohesivePacket&&, const vanetza::EthernetHeader&);
+    void update_position_vector();
+    void update_packet_flow(const vanetza::geonet::LongPositionVector&);
+
+    std::unordered_map<vanetza::btp::port_type, ApplicationParser*> m_app_layer_parsers;
+    vanetza::geonet::MIB mib_;
+    vanetza::PositionProvider& positioning_;
+    std::list<ApplicationParser*> app_parsers_;
+};
+
+
+#endif /* IDPS_CONTEXT_HPP */
